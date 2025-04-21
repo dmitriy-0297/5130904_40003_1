e8e508547568d8d2d45798e6f0cded77565c085c
sonabrv <abramenko6677@icloud.com>
2025-04-21 15:55:20 +0300

    corrections

--- abramenko.sofia/T2/main.cpp
+++ abramenko.sofia/T2/main.cpp
@@ -4,62 +4,109 @@
 #include <iterator>
 #include <algorithm>
 #include <string>
-#include <iomanip>
 
 //(:key1 89ull:key2 0b1000101:key3 "Data":)
 
-namespace nspace
-{
-	struct DataStruct {
-		unsigned long long key1 = 0;
-		unsigned long long key2 = 0;
-		std::string key3;
-	};
+struct DataStruct {
+	unsigned long long key1 = 0;
+	unsigned long long key2 = 0;
+	std::string key3;
+};
 
-	struct Delimiter {
-		std::string exp;
-	};
 
-	struct UllLit {
-		unsigned long long& ref;
-	};
+std::istream& operator>>(std::istream& in, DataStruct& data) {
+	//(:key1 89ull:key2 0b1000101:key3 "Data":)
 
-	struct UllBin {
-		unsigned long long& ref;
-	};
+	std::string row;
+	if (!std::getline(in, row)) return in;
 
-	struct String {
-		std::string& ref;
-	};
-
-	struct Label {
-		std::string exp;
-	};
-
-	std::istream& operator>>(std::istream& in, const Delimiter& dest);
-	std::istream& operator>>(std::istream& in, UllLit&& dest);
-	std::istream& operator>>(std::istream& in, UllBin&& dest);
-	std::istream& operator>>(std::istream& in, String&& dest);
-	std::istream& operator>>(std::istream& in, Label&& dest);
-	std::istream& operator>>(std::istream& in, DataStruct& dest);
-	std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
-
-	bool different(const DataStruct& a, const DataStruct& b) {
-		if (a.key1 != b.key1) return a.key1 < b.key1;
-		if (a.key2 != b.key2) return a.key2 < b.key2;
-		return a.key3.size() < b.key3.size();
+	size_t pos = row.find("key1");
+	if (pos == std::string::npos) {
+		in.setstate(std::ios::failbit);
+		return in;
 	}
+	size_t begin = row.find(' ', pos) + 1;
+	size_t end = row.find('u', begin);
+	std::string str1 = row.substr(begin, end - begin);
+	try {
+		data.key1 = std::stoull(str1);
+	}
+	catch (...) {
+		in.setstate(std::ios::failbit);
+		return in;
+	}
+
+	pos = row.find("key2");
+	if (pos == std::string::npos) {
+		in.setstate(std::ios::failbit);
+		return in;
+	}
+	begin = row.find(' ', pos) + 1;
+	end = row.find(':', begin);
+	std::string str2 = row.substr(begin + 2, end - (begin + 2));
+	try {
+		data.key2 = std::stoull(str2, nullptr, 2);
+	}
+	catch (...) {
+		in.setstate(std::ios::failbit);
+		return in;
+	}
+
+	pos = row.find("key3");
+	if (pos == std::string::npos) {
+		in.setstate(std::ios::failbit);
+		return in;
+	}
+	begin = row.find('"', pos) + 1;
+	end = row.find('"', begin);
+	std::string str3 = row.substr(begin, end - begin);
+	data.key3 = str3;
+
+	return in;
 }
+std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
+	//(:key1 89ull:key2 0b1000101:key3 "Data":)
+	out << "(:key1 " << data.key1 << "ull";
+	out << ":key2 0b";
+
+	unsigned long long a = data.key2;
+	if (a == 0) {
+		out << "0";
+	}
+	else {
+		int binary[32];
+		int i = 0;
+		while (a > 0) {
+			binary[i] = a % 2;
+			a = a / 2;
+			i++;
+		}
+		for (int j = i - 1; j >= 0; j--) {
+			out << binary[j];
+		}
+	}
+	out << ":key3 \"" << data.key3 << "\":)";
+
+	return out;
+}
+
+bool different(const DataStruct& a, const DataStruct& b) {
+	if (a.key1 != b.key1) return a.key1 < b.key1;
+	if (a.key2 != b.key2) return a.key2 < b.key2;
+	return a.key3.size() < b.key3.size();
+}
+
 
 int main()
 {
 	//(:key1 89ull:key2 0b1000101:key3 "Data":)
 
-	using nspace::DataStruct;
-
 	std::vector<DataStruct> data;
 
-	std::istringstream iss("(:key1 89ull:key2 0b1000101:key3 \"Data\":)(:key2 0b110:key1 42ull:key3 \"Another Data\":)");
+	std::istringstream iss("(:key1 89ull:key2 0b1000101:key3 \"Data\":)\n"
+		"(:key2 0b110:key1 42ull:key3 \"Another Data\":)\n"
+		"(:key2 0b110:key1 89ull:key3 \"Another Data\":)\n"
+		"(:key1 42ull:key2 0b110:key3 \"Data\":)\n");
 
 	std::copy(
 		std::istream_iterator<DataStruct>(iss),
@@ -67,9 +114,7 @@ int main()
 		std::back_inserter(data)
 	);
 
-	std::sort(data.begin(), data.end(), nspace::different);
-
-	std::cout << "Data:\n";
+	std::sort(data.begin(), data.end(), different);
 
 	std::copy(
 		data.begin(),
@@ -78,138 +123,4 @@ int main()
 	);
 
 	return 0;
-}
-
-namespace nspace
-{
-	std::istream& operator>>(std::istream& in, const Delimiter& dest) {
-		std::istream::sentry sentry(in);
-		if (!sentry) {
-			return in;
-		}
-		std::string input;
-		in >> input;
-		if (in && (input != dest.exp)) {
-			in.setstate(std::ios::failbit);
-		}
-		return in;
-	}
-
-	std::istream& operator>>(std::istream& in, UllLit&& dest) {
-		std::istream::sentry sentry(in);
-		if (!sentry)
-		{
-			return in;
-		}
-		return in >> dest.ref >> Delimiter{ "ull" };
-	}
-
-	std::istream& operator>>(std::istream& in, UllBin&& dest) {
-		std::istream::sentry sentry(in);
-		if (!sentry) {
-			return in;
-		}
-
-		Delimiter delimiter{ "0b" };
-
-		if (!(in >> delimiter)) {
-			return in;
-		}
-
-		std::string binaryStr;
-		in >> binaryStr;
-
-		try {
-			dest.ref = std::stoull(binaryStr, nullptr, 2);
-		}
-		catch (const std::invalid_argument&) {
-			in.setstate(std::ios::failbit);
-		}
-		catch (const std::out_of_range&) {
-			in.setstate(std::ios::failbit);
-		}
-
-		return in;
-	}
-
-	std::istream& operator>>(std::istream& in, String&& dest) {
-		std::istream::sentry sentry(in);
-		if (!sentry) {
-			return in;
-		}
-
-		Delimiter delimiter{ "\"" };
-
-		if (!(in >> delimiter)) {
-			return in;
-		}
-
-		std::getline(in, dest.ref, '"');
-
-		return in;
-	}
-
-	std::istream& operator>>(std::istream& in, Label&& dest) {
-		std::istream::sentry sentry(in);
-		if (!sentry)
-		{
-			return in;
-		}
-		std::string data = "";
-		if ((in >> String{ data }) && (data != dest.exp))
-		{
-			in.setstate(std::ios::failbit);
-		}
-		return in;
-	}
-
-	std::istream& operator>>(std::istream& in, DataStruct& dest) {
-		std::istream::sentry sentry(in);
-		if (!sentry) {
-			return in;
-		}
-
-		DataStruct temp;
-		char c;
-		in >> Delimiter{ "(:" };
-
-		std::string key;
-		while (in >> key && key != ":)") {
-			if (key == "key1") {
-				in >> UllLit{ temp.key1 } >> Delimiter{ ":" };
-			}
-			else if (key == "key2") {
-				in >> UllBin{ temp.key2 } >> Delimiter{ ":" };
-			}
-			else if (key == "key3") {
-				in >> String{ temp.key3 } >> Delimiter{ ":" };
-			}
-			else {
-				in.setstate(std::ios::failbit);
-				return in;
-			}
-		}
-
-		if (in) {
-			dest = temp;
-		}
-		return in;
-	}
-
-	std::ostream& operator<<(std::ostream& out, const DataStruct& src) {
-		std::ostream::sentry sentry(out);
-		if (!sentry)
-		{
-			return out;
-		}
-
-		//(:key1 89ull:key2 0b1000101:key3 "Data":)
-
-		out << "(:";
-		out << "key1 " << src.key1 << "ull";
-		out << ":key2 " << "0b" << src.key2;
-		out << ":key3 " << src.key3;
-		out << ":)";
-		return out;
-	}
 }
